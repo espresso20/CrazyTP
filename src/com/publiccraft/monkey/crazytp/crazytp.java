389fb22deda5ef83e7cdd5cc850311512b978e28
monkeypuddles <orion186@gmail.com>
2013-04-17 18:21:10 -0700

    notes

--- src/com/publiccraft/monkey/crazytp/crazytp.java
+++ src/com/publiccraft/monkey/crazytp/crazytp.java
@@ -10,7 +10,7 @@ import org.bukkit.plugin.java.JavaPlugin;
  * To change this template use File | Settings | File Templates.
  */
 class crazytp extends JavaPlugin {
-
+    //Tp to any random location given a area
     public void onDisable() {
     }
 
