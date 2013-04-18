5a26e8fe11b65f5d35482fbe5a207effe8b27ad8
monkeypuddles <orion186@gmail.com>
2013-04-17 17:51:53 -0700

    first commit

---
+++ src/com/publiccraft/monkey/crazytp/crazytp.java
@@ -0,0 +1,21 @@
+package com.publiccraft.monkey.crazytp;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Monkey
+ * Date: 4/17/13
+ * Time: 3:55 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class crazytp extends JavaPlugin {
+
+    public void onDisable() {
+    }
+
+    public void onEnable() {
+    }
+
+
+}
