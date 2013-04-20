c800648e6978c58460d6ea1b887eb12cc660b835
monkeypuddles <orion186@gmail.com>
2013-04-20 10:29:26 -0700

    testing args and strings

--- src/com/publiccraft/monkey/crazytp/crazytp.java
+++ src/com/publiccraft/monkey/crazytp/crazytp.java
@@ -1,7 +1,10 @@
 package com.publiccraft.monkey.crazytp;
 
-import org.bukkit.event.EventPriority;
-import org.bukkit.plugin.PluginManager;
+import java.util.logging.Logger;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
@@ -10,19 +13,34 @@ import org.bukkit.plugin.java.JavaPlugin;
  * Date: 4/17/13
  * Time: 3:55 PM
  */
-public class crazytp extends JavaPlugin{
-
+public class crazytp extends JavaPlugin
+{
+	public final Logger logger = Logger.getLogger("Minecraft");
+	
     public void onEnable()
     {
-        getLogger().info("onEnable has been invoked");
-        System.out.println("CrazyTP Enabled");
-
+        logger.info("CrazyTP Enabled");
     }
 
     public void onDisable()
     {
-        getLogger().info("onDisable has been invoked");
-        System.out.println("CrazyTP Disabled");
-
+        logger.info("CrazyTP Disabled");
     }
+    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args)
+    {
+    	// /pc heal
+    	
+    	if(commandLabel.equalsIgnoreCase("pc"))
+    	{
+    		if (args.length == 0 || args.length > 1)
+    		{
+    			sender.sendMessage(ChatColor.DARK_RED + "Incorrect number of arguments!");
+    		}
+    		
+    		return true;
+    	}
+    	
+    	return false;
+    }
+    
 }
